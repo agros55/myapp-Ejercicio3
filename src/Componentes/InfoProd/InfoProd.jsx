66535d3fc0dcf91784b65cd787ef7fb7a7dfb444
agros55 <fernandoviveros95@gmail.com>
2023-11-09 16:25:01 -0300

    #

--- src/Componentes/InfoProd/InfoProd.jsx
+++ src/Componentes/InfoProd/InfoProd.jsx
@@ -1,10 +1,8 @@
 import { useParams } from "react-router-dom";
 import Products from '../Productos/data';
 const InfoProd = () => {
-    let { id } = useParams();
-    console.log(id);
+    let { id } = useParams();;
     let Product = Products.find(prod => prod.Id == id);
-    console.log(Product);
     return (
         <div className="InfoProd">
             <h1>{Product.Nombre}</h1>
--- src/Componentes/Nav/Nav.jsx
+++ src/Componentes/Nav/Nav.jsx
@@ -2,7 +2,7 @@
 const Nav = () => {
     return (
         <nav className='Nav'>
-            <a href="/">Inicio</a>
+            <a href="/myapp-Ejercicio3/">Inicio</a>
             <a href="https://github.com/agros55/myapp-Ejercicio3.git" target="_blank">Repositorio</a>
         </nav>
     );
