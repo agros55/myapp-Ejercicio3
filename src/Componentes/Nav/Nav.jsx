ced2bc140150cf4ef9ec7a0b5db7c1821cb1de02
agros55 <fernandoviveros95@gmail.com>
2023-11-09 12:43:29 -0300

    Link al repositorio

--- src/Componentes/Nav/Nav.jsx
+++ src/Componentes/Nav/Nav.jsx
@@ -3,7 +3,7 @@ const Nav = () => {
     return (
         <nav className='Nav'>
             <a href="/">Inicio</a>
-            <a href="/">Repositorio</a>
+            <a href="https://github.com/agros55/myapp-Ejercicio3.git" target="_blank">Repositorio</a>
         </nav>
     );
 }
